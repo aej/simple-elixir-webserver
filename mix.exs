4afff658381d9fa4268e5e4010a793de21d408c4
Andy Jones <andy@andyjones.co>
2023-08-05 11:56:00 +0100

    Simple Elixir webserver

---
+++ mix.exs
@@ -0,0 +1,29 @@
+defmodule SimpleServer.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :simple_server,
+      version: "0.1.0",
+      elixir: "~> 1.15",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {SimpleServer.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:bandit, "~> 0.7.7"},
+      {:plug, "~> 1.14"}
+    ]
+  end
+end
